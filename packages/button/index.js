6d14aa13fb79e333ec729444291e709231a85094
henwq <wenquan_he@163.com>
2019-05-15 21:40:11 +0800

    button 解读

--- packages/button/index.js
+++ packages/button/index.js
@@ -1,8 +1,9 @@
-import ElButton from './src/button';
+import ElButton from './src/button'
 
 /* istanbul ignore next */
+// 注册全局组件
 ElButton.install = function(Vue) {
-  Vue.component(ElButton.name, ElButton);
-};
+  Vue.component(ElButton.name, ElButton)
+}
 
-export default ElButton;
+export default ElButton
--- packages/message/src/main.js
+++ packages/message/src/main.js
@@ -1,72 +1,77 @@
-import Vue from 'vue';
-import Main from './main.vue';
-import { PopupManager } from 'element-ui/src/utils/popup';
-import { isVNode } from 'element-ui/src/utils/vdom';
-let MessageConstructor = Vue.extend(Main);
+import Vue from 'vue'
+import Main from './main.vue'
+import { PopupManager } from 'element-ui/src/utils/popup'
+import { isVNode } from 'element-ui/src/utils/vdom'
+let MessageConstructor = Vue.extend(Main)
 
-let instance;
-let instances = [];
-let seed = 1;
+let instance
+let instances = []
+let seed = 1
 
+/**
+ * @description:
+ * @param {Sting || Object}
+ * @return: message 组件示例
+ */
 const Message = function(options) {
-  if (Vue.prototype.$isServer) return;
-  options = options || {};
+  if (Vue.prototype.$isServer) return // 是否在服务端
+  options = options || {} // 需要的所有参数
   if (typeof options === 'string') {
     options = {
       message: options
-    };
+    }
   }
-  let userOnClose = options.onClose;
-  let id = 'message_' + seed++;
+  let userOnClose = options.onClose
+  let id = 'message_' + seed++
 
   options.onClose = function() {
-    Message.close(id, userOnClose);
-  };
+    Message.close(id, userOnClose)  
+  }
   instance = new MessageConstructor({
     data: options
-  });
-  instance.id = id;
+  })
+  instance.id = id
   if (isVNode(instance.message)) {
-    instance.$slots.default = [instance.message];
-    instance.message = null;
+    instance.$slots.default = [instance.message]
+    instance.message = null
   }
-  instance.vm = instance.$mount();
-  document.body.appendChild(instance.vm.$el);
-  instance.vm.visible = true;
-  instance.dom = instance.vm.$el;
-  instance.dom.style.zIndex = PopupManager.nextZIndex();
-  instances.push(instance);
-  return instance.vm;
-};
+  instance.vm = instance.$mount()
+  document.body.appendChild(instance.vm.$el)
+  instance.vm.visible = true
+  instance.dom = instance.vm.$el
+  instance.dom.style.zIndex = PopupManager.nextZIndex()
+  instances.push(instance)
+  return instance.vm
+}
 
-['success', 'warning', 'info', 'error'].forEach(type => {
+;['success', 'warning', 'info', 'error'].forEach(type => {
   Message[type] = options => {
     if (typeof options === 'string') {
       options = {
         message: options
-      };
+      }
     }
-    options.type = type;
-    return Message(options);
-  };
-});
+    options.type = type
+    return Message(options)
+  }
+})
 
 Message.close = function(id, userOnClose) {
   for (let i = 0, len = instances.length; i < len; i++) {
     if (id === instances[i].id) {
       if (typeof userOnClose === 'function') {
-        userOnClose(instances[i]);
+        userOnClose(instances[i])
       }
-      instances.splice(i, 1);
-      break;
+      instances.splice(i, 1)
+      break
     }
   }
-};
+}
 
 Message.closeAll = function() {
   for (let i = instances.length - 1; i >= 0; i--) {
-    instances[i].close();
+    instances[i].close()
   }
-};
+}
 
-export default Message;
+export default Message
